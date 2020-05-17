f70279bc83094aecae451d2c886a4b164799696d
v1r4m <yslim1203@naver.com>
2020-05-17 10:46:47 +0900

    hr-note

---
+++ Assets/RhythmTool/Examples/Scripts/Line.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace RhythmTool.Examples
+{
+    public class Line : MonoBehaviour
+    {
+        public float timestamp { get; private set; }
+        public float opacity { get; private set; }
+
+        public void Init(Color color, float opacity, float timestamp)
+        {
+            this.timestamp = timestamp;
+            this.opacity = opacity;
+
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+            color = Color.Lerp(Color.clear, color, opacity);
+            meshRenderer.material.SetColor("_Color", color);
+        }
+    }
+}
--- Assets/hrscript.cs
+++ Assets/hrscript.cs
@@ -1,57 +1,73 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using SocketIOClient;
+using System.Diagnostics;
 
 public class hrscript : MonoBehaviour
-{
+    {
+    private float thres;
+    private string datastring;
+    private float dataint;
+    private int datashow;
+    List<string> buffer = new List<string>();
     // Start is called before the first frame update
-    string url = "http://127.0.0.1:999/";
-    public static Client Socket { get; private set; }
+    //        string url = "http://127.0.0.1:999/";
+    //       public static Client Socket { get; private set; }
     public Text txt;
 
-    void Awake()
-    {
-        Debug.Log("Awake called");
-        Socket = new Client(url);
-        Socket.Opened += SocketOpened;
+        /*        void Awake()
+                {
+                    Debug.Log("Awake called");
+                    Socket = new Client(url);
+                    Socket.Opened += SocketOpened;
 
-        Socket.Connect();
-        Debug.Log("Socket connect function");
-    }
-    private void SocketOpened(object sender, System.EventArgs e)
-    {
-        Debug.Log("Socket Opened");
-    }
+                    Socket.Connect();
+                    Debug.Log("Socket connect function");
+                }
+                private void SocketOpened(object sender, System.EventArgs e)
+                {
+                    Debug.Log("Socket Opened");
+                }*/
 
 
-    void Start()
-    {
-        txt = GameObject.Find("HRtext").GetComponent<Text>();
-        txt.text = "called";
-        Debug.Log("Start called");
-        Socket.Emit("Msg", "heeellllo");
-        Socket.On("MsgRes", (data) =>
+
+
+        void Start()
         {
-            Debug.Log(data.Json.args[0]);
-            txt.text = "heart rate changed: " + data.Json.args[0];
-        });
-    }
 
-    public void test()
+        /*            txt = GameObject.Find("HRtext").GetComponent<Text>();
+                    txt.text = "called";
+                    Debug.Log("Start called");
+                    Socket.Emit("Msg", "heeellllo");
+                    Socket.On("MsgRes", (data) =>
+                    {
+                        Debug.Log(data.Json.args[0]);
+                    });*/
+        /*            SocketManager.Socket.On("MsgRes", (data) =>
+                    {
+                        Debug.Log(data.Json.args[0]);
+                    });*/
+        txt = GameObject.Find("HRtext").GetComponent<Text>();
+        SocketManager.Socket.On("hra", (data) =>
+                    {
+                        UnityEngine.Debug.Log(data.Json.args[0]);
+//                        txt.text = "heart rate changed: " + data.Json.args[0];
+                        datastring = data.Json.args[0].ToString();
+                        datashow = (Convert.ToInt32(datastring));
+                        dataint = (Convert.ToInt32(datastring)-35);
+                        thres = dataint/500;
+//                        UnityEngine.Debug.Log(thres);
+                        RhythmTool.Examples.Visualizer.hr = Convert.ToSingle(0.06 + thres);
+                    });
+                    
+    }
+    void Update()
     {
-        //        Socket.Emit
+        txt.text = "heart rate changed: "+datashow;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        txt = GameObject.Find("HRtext").GetComponent<Text>();
-        Socket.On("hr", (data) =>
-        {
-            Debug.Log(data.Json.args[0]);
-            txt.text = "heart rate changed: " + data.Json.args[0];
-        });
-    }
 }
